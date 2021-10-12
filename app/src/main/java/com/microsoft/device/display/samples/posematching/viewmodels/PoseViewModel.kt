9ae0424c920b04c3fbd9f3e2490e4bea8cc23d1e
Kristen Halper <khalp@vt.edu>
2021-10-12 13:05:35 -0400

    Add basic pose angle comparisons

--- app/src/main/java/com/microsoft/device/display/samples/posematching/viewmodels/PoseViewModel.kt
+++ app/src/main/java/com/microsoft/device/display/samples/posematching/viewmodels/PoseViewModel.kt
@@ -6,17 +6,20 @@ import android.util.Log
 import androidx.camera.core.ImageProxy
 import androidx.lifecycle.ViewModel
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.pose.Pose
 import com.google.mlkit.vision.pose.PoseDetection
 import com.google.mlkit.vision.pose.PoseDetector
 import com.google.mlkit.vision.pose.defaults.PoseDetectorOptions
 import com.microsoft.device.display.samples.posematching.R
 import com.microsoft.device.display.samples.posematching.utils.GraphicOverlay
 import com.microsoft.device.display.samples.posematching.utils.PoseGraphic
+import com.microsoft.device.display.samples.posematching.utils.comparePoses
 import java.util.*
 
 class PoseViewModel : ViewModel() {
     private val poseDetector: PoseDetector
     var screenshot = false
+    private lateinit var referencePose: Pose
 
     init {
         val options = PoseDetectorOptions.Builder()
@@ -42,9 +45,22 @@ class PoseViewModel : ViewModel() {
 
     fun analyzeImage(resources: Resources, graphicOverlay: GraphicOverlay, image: InputImage = InputImage.fromBitmap(
         BitmapFactory.decodeResource(resources, R.drawable.bill),
-        0
-    ), imageProxy: ImageProxy? = null
-    ) {
+        0),
+        imageProxy: ImageProxy? = null
+    ): Boolean {
+        var returnVal = false
+
+
+        // REVISIT: actually take from reference frag
+        poseDetector.process(
+            InputImage.fromBitmap(
+                BitmapFactory.decodeResource(
+                    resources,
+                    R.drawable.bill
+                ), 0
+            )
+        ).addOnSuccessListener { referencePose = it }
+
         poseDetector.process(image)
             .addOnSuccessListener { results ->
                 // Task completed successfully
@@ -73,6 +89,11 @@ class PoseViewModel : ViewModel() {
 
                 Log.d("PoseTest", "Success!")
                 imageProxy?.close()
+
+                if (comparePoses(referencePose, results)) {
+                    Log.d("PoseTest", "Poses match!")
+                    returnVal = true
+                }
             }
             .addOnFailureListener { e ->
                 // Task failed with an exception
@@ -80,6 +101,7 @@ class PoseViewModel : ViewModel() {
                 Log.d("PoseTest", "Boo, failure", e)
                 imageProxy?.close()
             }
+        return returnVal
     }
 }
 
