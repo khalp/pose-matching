58c5ad1650b348df60f5ea740512902f3ad5f6f6
Kristen Halper <khalp@vt.edu>
2021-10-13 16:47:55 -0400

    adjust switch appearance

--- app/src/main/java/com/microsoft/device/display/samples/posematching/ui/view/AccessibilityMenu.kt
+++ app/src/main/java/com/microsoft/device/display/samples/posematching/ui/view/AccessibilityMenu.kt
@@ -3,8 +3,12 @@ package com.microsoft.device.display.samples.posematching.ui.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Switch
+import androidx.compose.material.SwitchDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -12,18 +16,23 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.microsoft.device.display.samples.posematching.ui.theme.PoseMatchingTheme
 
 @Composable
 fun AccessibilityMenu() {
-    var checkElbows by remember { mutableStateOf(false) }
-    var checkShoulders by remember { mutableStateOf(false) }
-    var checkKnees by remember { mutableStateOf(false) }
-    var checkHips by remember { mutableStateOf(false) }
+    var checkElbows by remember { mutableStateOf(true) }
+    var checkShoulders by remember { mutableStateOf(true) }
+    var checkKnees by remember { mutableStateOf(true) }
+    var checkHips by remember { mutableStateOf(true) }
 
     PoseMatchingTheme {
-        Column {
+        Column(
+            modifier = Modifier.wrapContentWidth(),
+            horizontalAlignment = Alignment.End
+        ) {
             SwitchWithLabel("elbows", checkElbows) { value -> checkElbows = value }
             SwitchWithLabel("shoulders", checkShoulders) { value -> checkShoulders = value }
             SwitchWithLabel("knees", checkKnees) { value -> checkKnees = value }
@@ -35,13 +44,19 @@ fun AccessibilityMenu() {
 @Composable
 private fun SwitchWithLabel(label: String, checked: Boolean, updateValue: (Boolean) -> Unit) {
     Row(
-        horizontalArrangement = Arrangement.Start,
+        horizontalArrangement = Arrangement.End,
         verticalAlignment = Alignment.CenterVertically,
     ) {
         Text(text = "check $label", color = MaterialTheme.colors.onSurface)
+        Spacer(Modifier.width(10.dp))
         Switch(
             checked = checked,
             onCheckedChange = updateValue,
+            colors = SwitchDefaults.colors(
+                checkedThumbColor = MaterialTheme.colors.secondary,
+                checkedTrackColor = MaterialTheme.colors.secondary,
+                uncheckedThumbColor = MaterialTheme.colors.secondary,
+            ),
         )
     }
 }
