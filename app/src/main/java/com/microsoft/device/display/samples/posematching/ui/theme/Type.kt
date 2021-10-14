0409c90191533e3f82fbde1b703b902c1ed26b0b
Kristen Halper <khalp@vt.edu>
2021-10-14 18:10:06 -0400

    Add our spooky font B-)

--- app/src/main/java/com/microsoft/device/display/samples/posematching/ui/theme/Type.kt
+++ app/src/main/java/com/microsoft/device/display/samples/posematching/ui/theme/Type.kt
@@ -2,29 +2,33 @@ package com.microsoft.device.display.samples.posematching.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.microsoft.device.display.samples.posematching.R
+
+val akayaFamily = FontFamily(Font(R.font.akaya_kanadaka_regular))
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = akayaFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     ),
     h1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = akayaFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 70.sp
     ),
     h2 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = akayaFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 30.sp
     ),
     h3 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = akayaFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 20.sp,
     )
